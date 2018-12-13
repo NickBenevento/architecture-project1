69b5500953142054ca179438423821e631e2525d
Nicholas Benevento <nbenevento@shell01.seas.gwu.edu>
2018-12-12 23:32:58 -0500

    commenting out a method

--- project6/kernels.c
+++ project6/kernels.c
@@ -325,8 +325,8 @@ void my_smooth(int dim, pixel *src, pixel *dst, int *rusage_time, unsigned long 
 		for (j = 0; j < dim; j++) {
 			pixel_sum *new = &sum;
 			new->red = new->green = new->blue = new->num = 0;
-			initialize_pixel_sum(&sum);
-			for(ii = ( (i-1) > 0 ? (i-1) : 0 ); ii <= ( (i+1) < (dim-1) ? (i+1) : (dim-1) ); ii++) 
+			//initialize_pixel_sum(&sum);
+			for(ii = ( (i-1) > 0 ? (i-1) : 0 ); ii <= ( (i+1) < (dim-1) ? (i+1) : (dim-1) ); ii++) {
 				for(jj = maximum(j-1, 0); jj <= minimum(j+1, dim-1); jj++) {
 					pixel p = src[ii*dim+jj];
 					new->red += (int) p.red;
@@ -335,7 +335,7 @@ void my_smooth(int dim, pixel *src, pixel *dst, int *rusage_time, unsigned long 
 					new->num++;
 					//accumulate_sum(&sum, src[ii*dim+jj]);
 				}
-					
+			}	
 
 			pixel *ptr_pixel = &current_pixel;
 			ptr_pixel->red = (unsigned short) (sum.red/sum.num);
